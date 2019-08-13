2ed6d00b3b6402d442d1128be93c81b44b4bc4b2
Matt Puchyr <matt.puchyr@gmail.com>
2019-08-13 03:51:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly_with_third_tier
                   :reading_railroad => {},
                   :pennsylvania_railroad => {},
                   :b_and_o_railroad => {},
-                  :short_line_railroad => {}
+                  :shortline_railroad => {}
                 }
   }}
   # When you start writing the implementation for this method, copy the Hash
