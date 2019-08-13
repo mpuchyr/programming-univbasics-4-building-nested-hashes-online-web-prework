ede9553f956fab87b016fccc59b4745fb835b880
Matt Puchyr <matt.puchyr@gmail.com>
2019-08-13 03:52:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,8 +16,8 @@ def monopoly_with_third_tier
                   :four_pieces_owned => 200
                 },
                 :names => {
-                  :reading_railroad => {},
-                  :pennsylvania_railroad => {},
+                  :reading_railroad => {"mortgage_value" => "$100"},
+                  :pennsylvania_railroad => {"mortgage_value" => "$100"},
                   :b_and_o_railroad => {},
                   :shortline_railroad => {}
                 }
