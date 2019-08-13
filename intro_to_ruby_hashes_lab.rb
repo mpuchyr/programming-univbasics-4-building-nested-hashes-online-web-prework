f7a04d7ed001230534762084a66d08056f32d93c
Matt Puchyr <matt.puchyr@gmail.com>
2019-08-13 02:55:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,8 +3,7 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  monopoly = {:railroads => {}}
-  monopoly[:pieces] = {}
+  monopoly = {:railroads => {:pieces => {}}}
 end
 
 def monopoly_with_third_tier
